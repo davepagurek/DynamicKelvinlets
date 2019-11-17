3880c9ac482978a527d8918a8761fc9ee2ec0b76
Dave Pagurek <dave@davepagurek.com>
2019-11-17 10:16:46 -0800

    Move function from header to .cpp

--- src/constants.h
+++ src/constants.h
@@ -3,7 +3,7 @@
 constexpr bool USE_STATIC_MESH = false;
 constexpr bool USE_SHADER = true;
 constexpr bool SHOW_KELVINLETS = false;
-constexpr int MAX_KELVINLETS = 60;
+constexpr int MAX_KELVINLETS = 100;
 constexpr int FRAME_RATE = 30;
-constexpr bool SAVE_SCREENSHOTS = true;
+constexpr bool SAVE_SCREENSHOTS = false;
 constexpr float MAX_KELVINLET_LIFE = 3; // seconds
