59ec369e3c5f2064dd6040d50926e7d2c7cd3bb4
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2019-11-16 18:10:49 -0800

    Add file naming so as to easily generate videos from ffmpeg.
    Example command I use to generate video from bin/data : ffmpeg -i %5d.png -c:v libx264 -r 30 -pix_fmt yuv420p Threshold4000Force5*1e-4.mp4

--- src/constants.h
+++ src/constants.h
@@ -5,5 +5,5 @@ constexpr bool USE_SHADER = true;
 constexpr bool SHOW_KELVINLETS = false;
 constexpr int MAX_KELVINLETS = 40;
 constexpr int FRAME_RATE = 30;
-constexpr bool SAVE_SCREENSHOTS = false;
+constexpr bool SAVE_SCREENSHOTS = true;
 constexpr float MAX_KELVINLET_LIFE = 3; // seconds
